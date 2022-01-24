b18203d664123368611ccc43435bd55dd66ddebb
Bruno Richard <bruno683@outlook.fr>
2022-01-24 16:35:23 +0100

    Adding templates EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -29,7 +29,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('bundles/EasyAdminBundle/layout.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -40,6 +40,17 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+
+            MenuItem::section('Blog'),
+            MenuItem::linkToCrud('Categories', 'fa fa-tags', Category::class),
+            MenuItem::linkToCrud('Blog Posts', 'fa fa-file-text', BlogPost::class),
+
+            MenuItem::section('Users'),
+            MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
+            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+        ];
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
