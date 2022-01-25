b02fe9cc7c5e95dc4c448645328c9926b6fb84c3
Bruno Richard <bruno683@outlook.fr>
2022-01-25 15:12:59 +0100

    Configuration photosEntity

--- src/Entity/Photos.php
+++ src/Entity/Photos.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PhotosRepository;
+use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: PhotosRepository::class)]
@@ -19,8 +20,8 @@ class Photos
     #[ORM\JoinColumn(nullable: false)]
     private $categorie;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $imageFile;
+    #[Vich\UploadableField(mapping: 'photos_images', fileNameProperty: 'imageName')]
+    private File $imageFile;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $imageName;
@@ -49,16 +50,29 @@ class Photos
         return $this;
     }
 
-    public function getImageFile(): ?string
-    {
-        return $this->imageFile;
-    }
-
-    public function setImageFile(string $imageFile): self
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
     {
         $this->imageFile = $imageFile;
 
-        return $this;
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): File
+    {
+        return $this->imageFile;
     }
 
     public function getImageName(): ?string
