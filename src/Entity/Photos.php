3a7c77718d935888f1196c126757dd079a43db4d
Bruno Richard <bruno683@outlook.fr>
2022-01-25 14:03:53 +0100

    Modified photoEntity

--- src/Entity/Photos.php
+++ src/Entity/Photos.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\PhotosRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PhotosRepository;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: PhotosRepository::class)]
+#[Vich\Uploadable]
 class Photos
 {
     #[ORM\Id]
@@ -13,29 +15,24 @@ class Photos
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $name;
-
     #[ORM\ManyToOne(targetEntity: Categories::class, inversedBy: 'photos')]
     #[ORM\JoinColumn(nullable: false)]
     private $categorie;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $imageFile;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $imageName;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $updateAt;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
 
     public function getCategorie(): ?Categories
     {
@@ -45,6 +42,42 @@ class Photos
     public function setCategorie(?Categories $categorie): self
     {
         $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getImageFile(): ?string
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(string $imageFile): self
+    {
+        $this->imageFile = $imageFile;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(?\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
 
         return $this;
     }
