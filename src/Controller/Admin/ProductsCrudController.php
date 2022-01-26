b93f830eb31639a19238b0a2ba9c93681792124f
Bruno Richard <bruno683@outlook.fr>
2022-01-26 16:49:46 +0100

    Added description field in ProductAdminCrud

--- src/Controller/Admin/ProductsCrudController.php
+++ src/Controller/Admin/ProductsCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Products;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProductsCrudController extends AbstractCrudController
@@ -21,6 +22,7 @@ class ProductsCrudController extends AbstractCrudController
         return [
 
             TextField::new('name'),
+            TextareaField::new('description'),
             ImageField::new('image')->setBasePath('/upload/images')->onlyOnIndex(),
             TextField::new('imageFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
         ];
