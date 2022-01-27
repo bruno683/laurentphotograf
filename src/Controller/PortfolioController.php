e87f0c46f5b92b89bff2658fcfe4b164d3b908b9
Bruno Richard <bruno683@outlook.fr>
2022-01-27 11:20:53 +0100

    Make migration

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,9 @@ class PortfolioController extends AbstractController
     #[Route('/portfolio', name: 'portfolio')]
     public function index(): Response
     {
-
+        $photo = new Products();
+        if ($photo->getPortfolio()) {
+        }
         return $this->render('portfolio/index.html.twig', []);
     }
 }
