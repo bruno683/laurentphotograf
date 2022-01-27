3bc2ae81c82550b000e3681bf1835b2bd09d6d58
Bruno Richard <bruno683@outlook.fr>
2022-01-27 09:28:49 +0100

    PeventDefualt on rightclick event

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import 'bootstrap';
+//const img = document.getElementsByTagName('img');
 
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
 // start the Stimulus application
 import './bootstrap';
