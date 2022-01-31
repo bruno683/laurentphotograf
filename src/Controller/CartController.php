23690cac98cd6c05963956e3381b7e4a3e96bcd4
Bruno Richard <bruno683@outlook.fr>
2022-01-31 21:33:00 +0100

    Added cart page

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use SessionHandlerInterface;
 use App\Repository\TiragesRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'cart')]
-    public function index(Request $request, TiragesRepository $tirageRepo): Response
+    public function index(Request $request, TiragesRepository $tirageRepo)
     {
         $session = $request->getSession();
         $cart = $session->get('panier', []);
@@ -20,37 +22,51 @@ class CartController extends AbstractController
 
         foreach ($cart as $id => $quantity) {
             $cartWidthData[] = [
-                'tirage limité' => $tirageRepo->find($id),
+                'produit' => $tirageRepo->find($id),
                 'quantité' => $quantity
             ];
         }
-
         $total = 0;
 
         foreach ($cartWidthData as $item) {
-            $totalItem = $item['tirage limité']->getPrix() / 100 * $item['quantité'];
-            $total += $totalItem;
+            $totalItems = $item['produit']->getPrix() * $item['quantité'];
+            $total += $totalItems;
         }
-
-
         return $this->render('cart/index.html.twig', [
-            'items' => $cartWidthData,
-            'total' => $total
+            'total' => $total,
+            'items' => $cartWidthData
         ]);
     }
 
     #[Route('/cart/add/{id}', name: 'cart_add')]
-    public function add($id, Request $request)
+    public function add(int $id, Request $request): Response
     {
         $session = $request->getSession();
         $panier = $session->get('panier', []);
 
-        if (!empty($panier[$id])) {
+        if (!empty($panier)) {
             $panier[$id]++;
         } else {
             $panier[$id] = 1;
         }
 
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'cart_remove')]
+    public function removeItem(int $id, Request $request): Response
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier)) {
+            $panier[$id]--;
+        } else {
+            $panier[$id] = 1;
+        }
 
         $session->set('panier', $panier);
 
--- src/Entity/Tirages.php
+++ src/Entity/Tirages.php
@@ -108,4 +108,9 @@ class Tirages
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
