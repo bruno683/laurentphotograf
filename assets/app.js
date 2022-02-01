be7c746e3ce71160529fbbad6680a2012aae9e51
Bruno Richard <bruno683@outlook.fr>
2022-02-01 20:39:10 +0100

    Add button basket

--- assets/app.js
+++ assets/app.js
@@ -14,34 +14,19 @@ document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 const add = document.querySelectorAll("a.add");
-const remove = document.querySelectorAll("a.delete");
+
 const count = document.querySelector(".count");
 
 
 add.forEach(function(e) {
     e.addEventListener("click", (event) => {
         event.preventDefault();
-
-
         const url = e.href;
         axios.get(url).then(function(response) {
-            const totalItems = 0;
-            const item = response.data.quantité;
-            count.innerHTML = (totalItems + item);
+            count.innerHTML = response.data.quantité;
         });
     }, false);
 });
 
-
-
-/*remove.forEach(function(link) {
-    link.addEventListener("click", (event) => {
-        event.preventDefault();
-        const url = link.href;
-        axios.get(url).then(function(response) {
-            console.log(JSON.stringify(response.data));
-        });
-    });
-});*/
 // start the Stimulus application
 import './bootstrap';
