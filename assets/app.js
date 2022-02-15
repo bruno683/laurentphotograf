327c5c4ef608d86b2930f7b22c08ebeb60c0a00b
Bruno Richard <bruno683@outlook.fr>
2022-02-15 13:55:31 +0100

    Add paypal button

--- assets/app.js
+++ assets/app.js
@@ -47,5 +47,45 @@ remove.forEach(function(e) {
     });
 });
 
+//----------------------PAYPAL-------------------------------
+function initPayPalButton() {
+    paypal.Buttons({
+        style: {
+            shape: 'rect',
+            color: 'gold',
+            layout: 'vertical',
+            label: 'paypal',
+
+        },
+
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{ "amount": { "currency_code": "EUR", "value": 18.1, "breakdown": { "item_total": { "currency_code": "EUR", "value": 1 }, "shipping": { "currency_code": "EUR", "value": 17 }, "tax_total": { "currency_code": "EUR", "value": 0.1 } } } }]
+            });
+        },
+
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(orderData) {
+
+                // Full available details
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+                // Show a success message within this page, e.g.
+                const element = document.getElementById('paypal-button-container');
+                element.innerHTML = '';
+                element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+                // Or go to another URL:  actions.redirect('thank_you.html');
+
+            });
+        },
+
+        onError: function(err) {
+            console.log(err);
+        }
+    }).render('#paypal-button-container');
+}
+initPayPalButton();
+
 // start the Stimulus application
 import './bootstrap';
